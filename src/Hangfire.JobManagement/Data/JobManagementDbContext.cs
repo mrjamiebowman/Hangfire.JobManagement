208eedd9f155062c18b4aa8f49aa2079633e804d
Jamie Bowman <mrjamiebowman@gmail.com>
2024-06-07 07:59:42 -0500

    context

--- src/Hangfire.JobManagement/Data/JobManagementDbContext.cs
+++ src/Hangfire.JobManagement/Data/JobManagementDbContext.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.EntityFrameworkCore;
 
 namespace Hangfire.JobManagement.Data
 {
-    internal class JobManagementDbContext
+    internal class JobManagementDbContext : DbContext
     {
+
     }
 }
