b6d564d0122ba75d85032c9aa7c6a7dc8b6896df
mrjamiebowman <mrjamiebowman@gmail.com>
2024-06-07 18:31:18 -0500

    dbcontext

--- src/Hangfire.JobManagement/Data/JobManagementDbContext.cs
+++ src/Hangfire.JobManagement/Data/JobManagementDbContext.cs
@@ -1,7 +1,11 @@
-﻿namespace Hangfire.JobManagement.Data
+﻿using Hangfire.JobManagement.Data.Entities;
+using System.Collections.Generic;
+
+namespace Hangfire.JobManagement.Data
 {
     internal class JobManagementDbContext 
     {
+        //public DbSet<Setting> Settings { get; set; }
 
     }
 }
