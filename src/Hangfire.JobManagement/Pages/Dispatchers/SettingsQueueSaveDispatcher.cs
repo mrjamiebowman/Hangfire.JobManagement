65bbe5ac4fa6ee4ba0826ad833782f8b13bb650e
mrjamiebowman <mrjamiebowman@gmail.com>
2024-06-08 07:57:58 -0500

    started dispatcher for settings

---
+++ src/Hangfire.JobManagement/Pages/Dispatchers/SettingsQueueSaveDispatcher.cs
@@ -0,0 +1,35 @@
+﻿using Hangfire.Dashboard;
+using Hangfire.Storage;
+using System;
+using System.Threading.Tasks;
+
+namespace Hangfire.JobManagement.Pages.Dispatchers
+{
+    internal class SettingsQueueSaveDispatcher : IDashboardDispatcher
+    {
+        private readonly IStorageConnection _connection;
+
+        public SettingsQueueSaveDispatcher()
+        {
+            _connection = JobStorage.Current.GetConnection();
+        }
+
+        public async Task Dispatch(DashboardContext context)
+        {
+            if (!"GET".Equals(context.Request.Method, StringComparison.InvariantCultureIgnoreCase))
+            {
+                context.Response.StatusCode = 405;
+                return;
+            }
+
+            await Task.Delay(10);
+
+            throw new NotImplementedException();
+
+            //var periodicJob = new List<PeriodicJob>();
+            //periodicJob.AddRange(JobAgent.GetAllJobStopped());
+
+            //await context.Response.WriteAsync(JsonConvert.SerializeObject(periodicJob));
+        }
+    }
+}
---
+++ src/Hangfire.JobManagement/Pages/Dispatchers/SettingsSaveDispatcher.cs
@@ -0,0 +1,35 @@
+﻿using Hangfire.Dashboard;
+using Hangfire.Storage;
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Threading.Tasks;
+
+namespace Hangfire.JobManagement.Pages.Dispatchers
+{
+    internal class SettingsSaveDispatcher : IDashboardDispatcher
+    {
+        private readonly IStorageConnection _connection;
+
+        public SettingsSaveDispatcher()
+        {
+            _connection = JobStorage.Current.GetConnection();
+        }
+
+        public async Task Dispatch(DashboardContext context)
+        {
+            if (!"POST".Equals(context.Request.Method, StringComparison.InvariantCultureIgnoreCase)) {
+                context.Response.StatusCode = 405;
+                return;
+            }
+
+            await Task.Delay(10);
+
+            throw new NotImplementedException();
+
+            //var periodicJob = new List<PeriodicJob>();
+            //periodicJob.AddRange(JobAgent.GetAllJobStopped());
+
+            //await context.Response.WriteAsync(JsonConvert.SerializeObject(periodicJob));
+        }
+    }
+}
