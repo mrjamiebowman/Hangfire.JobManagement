be03c62db37dea3225efab685377db4f5979740b
mrjamiebowman <mrjamiebowman@gmail.com>
2024-06-07 19:23:25 -0500

    clean up

--- src/Hangfire.JobManagement/Data/Entities/BatchOperation.cs
+++ src/Hangfire.JobManagement/Data/Entities/BatchOperation.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Hangfire.JobManagement.Data.Entities
+﻿namespace Hangfire.JobManagement.Data.Entities
 {
     internal class BatchOperation
     {
