5bf6fd4a8aa0025eb5b92632ce38de6692681cda
mrjamiebowman <jamie@redoma.digital>
2026-02-24 23:32:28 -0600

    docker updates

--- .docker/mssql/db-init.sh
+++ .docker/mssql/db-init.sh
@@ -6,6 +6,6 @@ sleep 45s
 echo "[+] Running SQL Setup Script"
 
 # run the setup script to create the DB and the schema in the DB
-/opt/mssql-tools18/bin/sqlcmd -S localhost -U sa -P 5yyEj47ZlXITk39o9#r -d master -i /appdev/sql/db.sql
+/opt/mssql-tools18/bin/sqlcmd -S localhost -C -U sa -P 5yyEj47ZlXITk39o9#r -d master -i /opt/scripts/db.sql
 
 exit 0
